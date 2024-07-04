33d5087e23d84eaae20082d7e9187999d141ca8a
이승구 <leesg107@naver.com>
2024-07-04 21:51:28 +0900

    feat: server2 spring web개발

--- server2/execute.sh
+++ server2/execute.sh
@@ -1,7 +1,13 @@
-.PHONY: run
+#!/bin/sh
 
-run:
-	@echo "Running ./gradlew docker"
-	@./gradlew clean
-    @./gradlew bootBuildImage --imageName=server2:latest
-    @docker run --network load-balance-network -p 9002:8080 server2:latest
+./gradlew clean
+
+docker stop server2 || true
+
+docker rm server2 || true
+
+docker rmi server2:latest || true
+
+./gradlew bootBuildImage --imageName=server2:latest
+
+docker run --network load-balance-network --name server2 -d -p 9002:8080 server2:latest
